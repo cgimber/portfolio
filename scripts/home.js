da53341bd9d462d55d09e150fe9bbae672b68aad
Christian Gimber <cgimber@ucla.edu>
2016-03-23 02:18:13 -0700

    mobile-friendly homepage

--- scripts/home.js
+++ scripts/home.js
@@ -3,8 +3,7 @@
 var isMobile = false;
 if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4)))
     isMobile = true;
-
-console.log(isMobile);
+// console.log(isMobile);
 
 /* preloader
 -------------------------------------------------------------------------------------*/
@@ -21,91 +20,103 @@ $(document).ready(function() {
     var gallery;
     var slideLinks = $('a.gallery-link');
 
-    $('#fullpage').fullpage({
-        // navigation
-        menu: false,
-        anchors: ['about', 'bruinbash', 'juggle', 'ageago', 'localsonly', 'sozo'],
-        //        navigation: false,
-        //        navigationPosition: 'right',
-        //        navigationTooltips: ['firstSlide', 'secondSlide'],
-        //        slidesNavigation: true,
-        //        slidesNavPosition: 'bottom',
+    if (isMobile) {
+        $('.section').addClass('isMobile');
+        gallery = $('.gallery').flickity({
+            // options
+            cellAlign: 'center',
+            percentPosition: true,
+            prevNextButtons: false,
+            pageDots: true,
+            wrapAround: true
+        });
+    } else {
+        $('#footer').hide();
+        $('#fullpage').fullpage({
+            // navigation
+            menu: false,
+            anchors: ['about', 'bruinbash', 'juggle', 'ageago', 'localsonly', 'sozo'],
+            //        navigation: false,
+            //        navigationPosition: 'right',
+            //        navigationTooltips: ['firstSlide', 'secondSlide'],
+            //        slidesNavigation: true,
+            //        slidesNavPosition: 'bottom',
 
-        // scrolling
-        css3: true,
-        scrollingSpeed: 700,
-        autoScrolling: true,
-        scrollBar: false,
-        easing: 'easeInQuart',
-        easingcss3: 'ease',
-        loopBottom: true,
-        loopTop: false,
-        loopHorizontal: true,
-        continuousVertical: false,
-        normalScrollElements: '#element1, .element2',
-        scrollOverflow: false,
-        touchSensitivity: 15,
-        normalScrollElementTouchThreshold: 5,
+            // scrolling
+            css3: true,
+            scrollingSpeed: 700,
+            autoScrolling: true,
+            scrollBar: false,
+            easing: 'easeInQuart',
+            easingcss3: 'ease',
+            loopBottom: true,
+            loopTop: false,
+            loopHorizontal: true,
+            continuousVertical: false,
+            normalScrollElements: '#element1, .element2',
+            scrollOverflow: false,
+            touchSensitivity: 15,
+            normalScrollElementTouchThreshold: 5,
 
-        // accessibility
-        keyboardScrolling: true,
-        animateAnchor: true,
+            // accessibility
+            keyboardScrolling: true,
+            animateAnchor: true,
 
-        // design
-        controlArrows: true,
-        verticalCentered: true,
-        resize: true,
-        sectionsColor: ['#1a1a1a', '#3F3F3F', '#14A697', '#f2bb13', '#F29D35', '#f25151', '#F25252'],
-        paddingTop: '80px',
-        //        paddingBottom: '0px',
-        //        fixedElements: '#header, .footer',
-        responsive: 0,
+            // design
+            controlArrows: true,
+            verticalCentered: true,
+            resize: true,
+            sectionsColor: ['#1a1a1a', '#3F3F3F', '#14A697', '#f2bb13', '#F29D35', '#f25151'],
+            paddingTop: '80px',
+            //        paddingBottom: '0px',
+            //        fixedElements: '#header, .footer',
+            responsive: 0,
 
-        // custom selectors
-        sectionSelector: '.section',
-        slideSelector: '.slide',
+            // custom selectors
+            sectionSelector: '.section',
+            slideSelector: '.slide',
 
-        // events
-        onLeave: function(index, nextIndex, direction) {},
-        afterLoad: function(anchorLink, index) {
-            section = anchorLink;
+            // events
+            onLeave: function(index, nextIndex, direction) {},
+            afterLoad: function(anchorLink, index) {
+                section = anchorLink;
 
-            $('ul#section-links a').removeClass('active');
-            if (section == "about")
-                $('#about-link').addClass('active');
-            else
-                $('#work-link').addClass('active');
+                $('ul#section-links a').removeClass('active');
+                if (section == "about")
+                    $('#about-link').addClass('active');
+                else
+                    $('#work-link').addClass('active');
 
-            if (index != 1)
-                updateSlideLinks();
-        },
-        afterRender: function() {
+                if (index != 1)
+                    updateSlideLinks();
+            },
+            afterRender: function() {
 
-            // use fallbacks for cases where section is undefined
-            if (!section) { // 1) afterLoad hasn't fired yet
-                section = document.URL.split('#')[1];
-                if (!section) // 2) the page was loaded without a page anchor 
-                    section = "about";
-            }
-            // if (!isMobile) {
+                // use fallbacks for cases where section is undefined
+                if (!section) { // 1) afterLoad hasn't fired yet
+                    section = document.URL.split('#')[1];
+                    if (!section) // 2) the page was loaded without a page anchor 
+                        section = "about";
+                }
                 gallery = $('.gallery').flickity({
                     // options
                     cellAlign: 'center',
                     percentPosition: true,
                     prevNextButtons: false,
                     pageDots: false,
+                    draggable: false,
                     wrapAround: true
                 });
-            // }
 
-            if (section == "about")
-                $('#about-link').addClass('active');
+                if (section == "about")
+                    $('#about-link').addClass('active');
 
-        },
-        afterResize: function() {},
-        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {},
-        onSlideLeave: function(anchorLink, index, slideIndex, direction) {}
-    });
+            },
+            afterResize: function() {},
+            afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {},
+            onSlideLeave: function(anchorLink, index, slideIndex, direction) {}
+        });
+    }
 
     $('[data-typer-targets]').OrderlyTyper();
 
